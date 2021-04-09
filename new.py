d363118f38fa54b4e5081580a49beb76de8af415
enryu-maker <akifkhan60067@gmail.om>
2021-04-10 01:23:31 +0530

    algo done

---
+++ new.py
@@ -0,0 +1,27 @@
+a=[1,2,3,45,6]
+addr=['a','b','c','d','e']
+comp_addr=['b','x','y','d','e']
+truth=[]
+ind_vlaue=[]
+for x in comp_addr:
+    if x in addr:
+        truth.append('true')
+        ind_vlaue.append(addr.index(x))
+    else:
+        truth.append('false')
+        ind_vlaue.append('na')
+b=[]
+for z in ind_vlaue:
+    if z!='na':
+        z=int(z)
+        b.append(a[z])
+    else:
+        b.append('na')
+
+print(addr)
+print(a)
+print(comp_addr)
+print(b)
+print(truth)
+print(ind_vlaue)
+'''print(b)'''
--- processing.py
+++ processing.py
@@ -1,9 +1,38 @@
-#!/usr/bin/python3
+
 import pandas as pd
 
 
 matrix=pd.read_csv('matrix.csv')
 main=pd.read_csv('main.csv')
 
-print(matrix.head())
-print(main.head())
+main_addr=main['main_addr'].values.tolist()
+copy_id=main['copy_id'].values.tolist()
+comp_addr=matrix['comp_addr'].values.tolist()
+replace_id=matrix['Property_ID'].values.tolist()
+
+def Lower(a,b):
+    c=[]
+    d=[]
+    for x in a :
+        x=x.lower().replace(' ','')
+        c.append(x)
+    for y in b:
+        y=y.lower().replace(' ','')
+        d.append(y)
+    return c,d
+main_addr1,comp_addr1=Lower(main_addr,comp_addr)
+def compare(a,b,c,d):
+    available=[]
+    index_value=[]
+    for x in a:
+        if x in b:
+            available.append('true')
+            ind=b.index(x)
+            index_value.append(ind)
+        else:
+            available.append('false')
+            index_value.append('NA')
+
+        
+
+
