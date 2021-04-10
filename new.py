cf630f918b00f7a090056e21c2e15eedf4362fd8
enryu-maker <akifkhan60067@gmail.om>
2021-04-10 09:48:54 +0530

    final testing

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 a=[1,2,3,45,6]
 #kira
-addr=['a','b','c','d','e']
-comp_addr=['b','x','d','e']
+addr=['12a','12b','10c','32d','90e']
+comp_addr=['12b','14x','32d','90e']
 truth=[]
 ind_vlaue=[]
 for x in comp_addr:
